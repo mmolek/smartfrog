7068a771d32bd42dc436f9b84f4fb71cc3c76b45
Matic Molek <mmolek@Matics-MBP.local>
2016-02-24 21:07:00 +0100

    Adding /version option

--- smartfrog.py
+++ smartfrog.py
@@ -1,9 +1,25 @@
-from flask import Flask
-application = Flask(__name__)
+import flask
+from shelljob import proc
 
-@application.route("/")
+app = flask.Flask(__name__)
+
+@app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There! Test tri!</h1>"
+    return "<h1 style='color:blue'>Hello There! Test lala!</h1>"
+
+
+@app.route( '/version' )
+def stream():
+    g = proc.Group()
+    p = g.run( [ "cat", ".git/refs/heads/master" ] )
+
+    def read_process():
+        while g.is_pending():
+            lines = g.readlines()
+            for proc, line in lines:
+                yield line
+
+    return flask.Response( read_process(), mimetype= 'text/plain' )
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
